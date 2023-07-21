c473a6d1e1d085a8049413b2abc833b0e80858ad
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-21 19:23:59 +0200

    fix for cicle

--- js/main.js
+++ js/main.js
@@ -7,7 +7,23 @@ const newDiv = document.getElementById("box-target");
 // CREAIMAO UN CICLO FOR PER GENERAZIONE NUMERI DA 1 A 100
 
 for (let i = 1; i <= 100; i++) {
-  let outpout = i;
+  let output = i;
   let myClass = "number";
   const myDiv = document.createElement("div");
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    myClass = "fizzbuzz";
+    output = "FizzBuzz";
+  } else if (i % 3 == 0) {
+    myClass = "fizz";
+    output = "Fizz";
+  } else if (i % 5 == 0) {
+    myClass = "buzz";
+    output = "Buzz";
+  }
+
+  myDiv.innerHTML = output;
+  myDiv.classList.add("classic__box");
+  myDiv.classList.add(myClass);
+  newDiv.append(myDiv);
 }
